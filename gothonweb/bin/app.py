38df71ad9839def7467eca721ab90b172ec64138
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-04-30 13:39:01 +0000

    Add hello_form, POST for index, and link on index#POST back to index#GET.

--- gothonweb/bin/app.py
+++ gothonweb/bin/app.py
@@ -1,17 +1,26 @@
-# import web
+import web
 
 urls = (
-    '/', 'index'
+  '/hello', 'Index'
 )
 
-# app = web.application(urls, globals())
+
+app = web.application(urls, globals())
 
 render = web.template.render('templates/')
 
-class index(object):
+class Index(object):
     def GET(self):
-        greeting = "Hello World"
-        return render.foo(greeting = greeting)
+        return render.hello_form()
 
-# if __name__ == "__main__":
-#     app.run()
+    def POST(self):
+        form = web.input(name="Nobody", greet=None)
+
+        if form.greet:
+            greeting = "%s, %s" % (form.greet, form.name)
+            return render.index(greeting = greeting)
+        else:
+            return "ERROR: greet is required."
+
+if __name__ == "__main__":
+    app.run()
